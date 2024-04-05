e51062a9b3fd257c1c7dfa6b52e4da528b208a3c
DNS <37093+DNS@users.noreply.github.com>
2024-04-05 23:13:13 +0700

    -

--- 2. Advanced/cmdletbinding.ps1
+++ 2. Advanced/cmdletbinding.ps1
@@ -1,7 +1,7 @@
 [cmdletbinding()]
 Param(
-    [Parameter(Mandatory=$true)] [string] $Argument1 = 'default value',
-    [string] $Argument2
+    [Parameter(Mandatory=$true)] [String] $Argument1 = 'default value',
+    [Int] $Argument2
 )
 
 'arg1: ' + $Argument1
@@ -11,5 +11,16 @@ Param(
 run:
 
 .\test.ps1 -Argument1 'test 123' -Argument2 888
+
+String 
+Int Int64 Int128
+UInt UInt64 UInt128
+BigInt
+Float Double
+
+[Int128]::MaxValue
+[UInt128]::MaxValue
+[Float]::MaxValue
+[Double]::MaxValue
 
 #>
